e3894550846bd39bc3bf13fee0ba79c2e4bb259d
Mounir Abdous <mounir.abdous@cortexmedia.ca>
2019-09-19 10:50:47 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 				<p>
-					Hi there!!!
+					Test changed updated
 				</p>
       </header>
     </div>
